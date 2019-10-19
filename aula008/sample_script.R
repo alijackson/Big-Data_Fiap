29f41a405cee2c6b8d4f270c7fd1e9f6e1f73e62
Logon Aluno <logonaluno@local.com>
2019-10-19 11:45:31 -0300

    Ultima Aula. 	Final de curso. 	Desafio com baby

--- aula008/sample_script.R
+++ aula008/sample_script.R
@@ -1,10 +1,37 @@
 # base script
+
+install.packages("randomForest")
+library('randomForest') # classification algorithm
+
+install.packages("e1071")
+library(e1071)
+
+install.packages("party")
+library(party)
+
+
 train <- read.csv("train.csv")
 test <- read.csv("test.csv")
 
-mod <- lm(venda~outmg, data=train)
+
+mod <- lm(desconto~margem, data=train)
+
+cor(train$desconto,train$margem)
+p <- predict(mod, newdata=test)
+summary(p)
+p <- as.data.frame(cbind(1:31,p))
+
+train$date
+
+boxplot(train$margem~train$weekday)
+summary(p)
+#names(p)<-c("id","venda")
+#write.csv(p, file="predict.csv", row.names=FALSE)
+
+
+mod <- lm(venda~desconto+margem, data=train)
 p <- predict(mod, newdata=test)
 
-p <- as.data.frame(cbind(1:31,p))
-names(p)<-c("id","venda")
-write.csv(p, file="predict.csv", row.names=FALSE)
+val <- read.csv("validate.csv")
+RMSE<-sqrt(mean((p-val$venda)^2))
+RMSE
